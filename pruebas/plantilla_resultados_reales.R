6152e6db9f451250564e69751b4f620b6b6a8f0a
adfmb <actuario.alfredo.mb@gmail.com>
2018-06-05 20:10:23 -0500

    Equipo_gsub en resultados_reales.csv

--- pruebas/plantilla_resultados_reales.R
+++ pruebas/plantilla_resultados_reales.R
@@ -11,13 +11,14 @@ doc$Partido<-as.character(doc$Partido)
 doc$E1E2<-as.character(doc$E1E2)
 doc$Equipo<-as.character(doc$Equipo)
 
-cols_ids<-c("Codigo","Grupo","Partido","E1E2","Equipo")
+cols_ids<-c("Codigo","Grupo","Partido","E1E2","Equipo","Equipo_gsub")
 cols_resultados<-c("GolesFavor","GolesContra","Pts","DiferenciaGoles","Ganado","Perdido","Empate")
 resultados_reales<-doc%>%
   select(one_of(c(cols_ids,cols_resultados)))%>%
   mutate_at(vars(one_of(cols_resultados)),to_zero)%>%
   arrange(Grupo,Partido,E1E2)%>%
   mutate(Equipo=replace(Equipo, Grupo=="W", "por_definir"),
+         Equipo_gsub=replace(Equipo_gsub, Grupo=="W", "por_definir"),
          status_juego="por_jugar")%>%
   rename(GolesFavor_real=GolesFavor,
          GolesContra_real=GolesContra,
