beb8cbd7c07ea7a8262c42ec25d3ce8e4c4102d5
adfmb <actuario.alfredo.mb@gmail.com>
2018-06-07 12:13:22 -0500

    cambios en streamingmatch

--- utils/streamingmatch.R
+++ utils/streamingmatch.R
@@ -1,23 +1,26 @@
 source("utils/generate_gbs_F01.R")
 library(dplyr)
 library(tidyr)
-streamingmatch<-function(Grupo_sm="A",Partido_sm="p1",alldocs=alldocs,resultados_reales=resultados_reales){
+streamingmatch<-function(alldocs=alldocs,resultados_reales=resultados_reales){
+  
+  reales_sm<-resultados_reales%>%
+    # filter(Grupo==Grupo_sm & Partido==Partido_sm)
+    filter(Grupo!="W" & status_juego=="jugando")
   
   prediccones_sm00<-alldocs%>%
-    filter(Grupo==Grupo_sm & Partido==Partido_sm)
-    
-  reales_sm<-resultados_reales%>%
-    filter(Grupo==Grupo_sm & Partido==Partido_sm)
+    # filter(Grupo==Grupo_sm & Partido==Partido_sm)  
+    right_join(reales_sm%>%
+                 select(Grupo,Partido,E1E2))
   
   gbs_sm00<-generate_gbs_F01(prediccones_sm00,reales_sm)
     
   prediccones_sm01<-prediccones_sm00%>%
-    select(nomconcursante,folio,Equipo_gsub,GolesFavor)%>%
-    spread(Equipo_gsub,GolesFavor,fill = 0)
+    select(nomconcursante,folio,Grupo,Partido,Equipo_gsub,GolesFavor)%>%
+    spread(Equipo_gsub,GolesFavor,fill = "-")
   
   pts_sm00<-gbs_sm00$gb_nomb_grupopartido%>%
     ungroup()%>%
-    select(nomconcursante,folio,Puntos_Resultado,Puntos_Marcador,Puntos_Partido)
+    select(nomconcursante,folio,Grupo,Partido,Puntos_Resultado,Puntos_Marcador,Puntos_Partido)
   
   sm<-prediccones_sm01%>%
     left_join(pts_sm00)
