b34f515331527eba10e14078cd5459e387be96f2
adfmb <actuario.alfredo.mb@gmail.com>
2018-06-07 21:20:39 -0500

    importa_alldocs como funcion

--- utils/importacion.R
+++ utils/importacion.R
@@ -1,24 +1,29 @@
-library(dplyr)
-docsnames<-list.files("data/")
-docsnames<-docsnames[grep("@",docsnames)]
-alldocs<-data.frame()
-
-for(docname in docsnames){
-  print(docname)
-  doc<-read.csv(paste0("data/",docname),header=T)
-  alldocs<-rbind(alldocs,doc)
+importa_alldocs<-function(){
+  
+  library(dplyr)
+  docsnames<-list.files("data/")
+  docsnames<-docsnames[grep("@",docsnames)]
+  alldocs<-data.frame()
+  
+  for(docname in docsnames){
+    print(docname)
+    doc<-read.csv(paste0("data/",docname),header=T)
+    alldocs<-rbind(alldocs,doc)
+    
+  }
+  
+  # View(alldocs)
+  alldocs$Grupo<-as.character(alldocs$Grupo)
+  alldocs$Partido<-as.character(alldocs$Partido)
+  alldocs$E1E2<-as.character(alldocs$E1E2)
+  alldocs$Equipo<-as.character(alldocs$Equipo)
+  alldocs$nomconcursante<-as.character(alldocs$nomconcursante)
+  alldocs$folio<-as.character(alldocs$folio)
+  
+  alldocs<-alldocs%>%
+    arrange(Grupo,Partido,E1E2)
+  
+  return(alldocs)
   
 }
-
-# View(alldocs)
-alldocs$Grupo<-as.character(alldocs$Grupo)
-alldocs$Partido<-as.character(alldocs$Partido)
-alldocs$E1E2<-as.character(alldocs$E1E2)
-alldocs$Equipo<-as.character(alldocs$Equipo)
-alldocs$nomconcursante<-as.character(alldocs$nomconcursante)
-alldocs$folio<-as.character(alldocs$folio)
-
-alldocs<-alldocs%>%
-  arrange(Grupo,Partido,E1E2)
-
-saveRDS(alldocs,"data/alldocs.rds")
+# saveRDS(alldocs,"data/alldocs.rds")
