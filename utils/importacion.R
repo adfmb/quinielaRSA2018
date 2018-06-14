51448397ba06e6d0ab938e596c223d7ad93015e0
adfmb <actuario.alfredo.mb@gmail.com>
2018-06-14 11:04:00 -0500

    overwrite importacion

--- utils/importacion.R
+++ utils/importacion.R
@@ -1,31 +1,39 @@
-importa_alldocs<-function(){
+importa_alldocs<-function(overwrite=F){
   
-  library(dplyr)
-  docsnames<-list.files("Quinielas_Grupo_A/")
-  docsnames<-docsnames[grep("@",docsnames)]
-  alldocs<-data.frame()
-  # options(warn=1)
-  for(docname in docsnames){
-    print(docname)
-    doc<-read.csv(paste0("Quinielas_Grupo_A/",docname),header=T)
-    doc<-doc%>%
-      mutate_at(.vars = c("Codigo","Grupo","Partido","E1E2","Equipo",
-                          "Equipo_gsub","nomequipo",
-                          "nomconcursante","folio","correo"),.funs = as.character)
-    alldocs<-bind_rows(alldocs,doc)#rbind(alldocs,doc)
+  if(overwrite){
     
+    library(dplyr)
+    docsnames<-list.files("Quinielas_Grupo_A/")
+    docsnames<-docsnames[grep("@",docsnames)]
+    alldocs<-data.frame()
+    # options(warn=1)
+    for(docname in docsnames){
+      print(docname)
+      doc<-read.csv(paste0("Quinielas_Grupo_A/",docname),header=T)
+      doc<-doc%>%
+        mutate_at(.vars = c("Codigo","Grupo","Partido","E1E2","Equipo",
+                            "Equipo_gsub","nomequipo",
+                            "nomconcursante","folio","correo"),.funs = as.character)
+      alldocs<-bind_rows(alldocs,doc)#rbind(alldocs,doc)
+      
+    }
+    
+    # View(alldocs)
+    alldocs$Grupo<-as.character(alldocs$Grupo)
+    alldocs$Partido<-as.character(alldocs$Partido)
+    alldocs$E1E2<-as.character(alldocs$E1E2)
+    alldocs$Equipo<-as.character(alldocs$Equipo)
+    alldocs$nomconcursante<-as.character(alldocs$nomconcursante)
+    alldocs$folio<-as.character(alldocs$folio)
+    
+    alldocs<-alldocs%>%
+      arrange(Grupo,Partido,E1E2)
+    
+    saveRDS(alldocs,"data/alldocs.rds")
+    
+  }else{
+    alldocs<-readRDS("data/alldocs.rds")
   }
-  
-  # View(alldocs)
-  alldocs$Grupo<-as.character(alldocs$Grupo)
-  alldocs$Partido<-as.character(alldocs$Partido)
-  alldocs$E1E2<-as.character(alldocs$E1E2)
-  alldocs$Equipo<-as.character(alldocs$Equipo)
-  alldocs$nomconcursante<-as.character(alldocs$nomconcursante)
-  alldocs$folio<-as.character(alldocs$folio)
-  
-  alldocs<-alldocs%>%
-    arrange(Grupo,Partido,E1E2)
   
   return(alldocs)
   
