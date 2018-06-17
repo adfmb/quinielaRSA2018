2de4dde4f6b81e4b09343d8dfcdeb6a574416ef3
adfmb <actuario.alfredo.mb@gmail.com>
2018-06-16 21:00:28 -0500

    read google sheets 01

---
+++ pruebas/read_gs01.R
@@ -0,0 +1,6 @@
+library(googlesheets)
+gs_auth(new_user = TRUE)
+gs_ls()
+for_gs <- gs_title("resultados_reales")
+for_gs2<-gs_key("1I-Fmc-rEsjqrKwmoddgf5vhoNpgtcfgb9lFy-I6SujM")
+for_gs_sheet <- gs_read(for_gs)
