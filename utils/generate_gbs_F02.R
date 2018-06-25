6fc29da4760cb34341080e30a1a3ce3d9c62ee7d
adfmb <actuario.alfredo.mb@gmail.com>
2018-06-24 19:35:28 -0500

    Joining, by = c(Codigo2, Equipo_gsub)

--- utils/generate_gbs_F02.R
+++ utils/generate_gbs_F02.R
@@ -6,12 +6,12 @@ generate_gbs_F2<-function(alldocs=alldocs,resultados_reales=resultados_reales,ma
     mutate_at(vars(Grupo,Partido,E1E2),.funs = funs(as.character))%>%
     filter(Grupo=="W" & Ganado==1)
   
-  resultados_reales_F01_pterminados<-resultados_reales%>%
+  resultados_reales_F02_pterminados<-resultados_reales%>%
     mutate_at(vars(Grupo,Partido,E1E2,Equipo_gsub,status_juego,Codigo2),.funs = funs(as.character))%>%
     filter(Grupo=="W" & status_juego!="por_jugar")# & Ganado_real==1)
   
   alldocs_selecF02_pterminados<-alldocs_selecF02%>%
-    right_join(resultados_reales_F01_pterminados%>%select(-Codigo,-Grupo,-Partido,-E1E2))%>%
+    right_join(resultados_reales_F02_pterminados%>%select(-Codigo,-Grupo,-Partido,-E1E2))%>% #Joining, by = c("Codigo2", "Equipo_gsub")
     arrange(Grupo,Partido,nomconcursante,E1E2)%>%
     mutate(Dif_GF=as.integer(GolesFavor-GolesFavor_real),
            I_Ganados=Ganado*Ganado_real,
