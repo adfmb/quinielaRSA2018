a144314467b70f5760f3fc8abc55146d9d42f877
adfmb <actuario.alfredo.mb@gmail.com>
2018-06-26 17:02:43 -0500

    Ganadores_Fase_acertados & Ganadores_TotalesF02_acertados

--- utils/generate_gbs_F02.R
+++ utils/generate_gbs_F02.R
@@ -39,25 +39,25 @@ generate_gbs_F02<-function(alldocs=alldocs,resultados_reales=resultados_reales,m
   ###  Agrupando por nomconcursante, folio & Codigo2 (Octavos, Cuartos, Semifinales, Tercer, Final)   -------------------------------FALTA EL IF_ELSE Fase="Grupos") ---- Por ahora esta funcion será solo para Fase de Grupos
   gb_nomb_codigo2<-alldocs_selecF02_pterminados%>%
     group_by(nomconcursante,folio,Codigo2)%>%
-    summarise(Resultado_correcto_partido_Ganador=as.integer(sum(I_Ganados)),#/2),
+    summarise(Ganadores_Fase_acertados=as.integer(sum(I_Ganados)),#/2),
               Resultado_correcto_partido_Perdedor=as.integer(sum(I_Perdido)))%>%#,
               # Resultado_correcto_partido_Empate=as.integer(sum(Resultado_correcto_equipo_Empate)/2),
               # min_Difgolesfavor=as.integer(min(Dif_GF)),
               # max_Difgolesfavor=as.integer(max(Dif_GF)),
     ungroup()%>%
     mutate(
-              Puntos_primlugar=if_else(Codigo2=="Final",8*Resultado_correcto_partido_Ganador,0),
+              Puntos_primlugar=if_else(Codigo2=="Final",8*Ganadores_Fase_acertados,0),
               Puntos_seglugar=if_else(Codigo2=="Final",4*Resultado_correcto_partido_Perdedor,0),
-              Puntos_terlugar=if_else(Codigo2=="Tercer",2*Resultado_correcto_partido_Ganador,0),
+              Puntos_terlugar=if_else(Codigo2=="Tercer",2*Ganadores_Fase_acertados,0),
               # Puntos_Resultado = 3*Resultado_correcto_partido_Ganador + 2*Resultado_correcto_partido_Empate,
               # Puntos_Marcador = if_else(Puntos_Resultado>0 & Marcador_correcto_partido==1,5,0),
-              Puntos_Partido = 5*Resultado_correcto_partido_Ganador + Puntos_primlugar+Puntos_seglugar+Puntos_terlugar
+              Puntos_Partido = 5*Ganadores_Fase_acertados + Puntos_primlugar+Puntos_seglugar+Puntos_terlugar
               )
   
   gb_nomb<-gb_nomb_codigo2%>%
     # ungroup()%>%
     group_by(nomconcursante,folio)%>%
-    summarise(Suma_Puntos_Resultado=sum(Resultado_correcto_partido_Ganador),
+    summarise(Ganadores_TotalesF02_acertados=sum(Ganadores_Fase_acertados),
               Suma_Puntos_1erL=sum(Puntos_primlugar),
               Suma_Puntos_2doL=sum(Puntos_seglugar),
               Suma_Puntos_3erL=sum(Puntos_terlugar),
