9882702a02f91c8d831b90e3d2766ab77bb69a51
adfmb <actuario.alfredo.mb@gmail.com>
2018-06-26 16:58:53 -0500

    cambios en streamingmatch_F02

--- utils/streamingmatch_F02.R
+++ utils/streamingmatch_F02.R
@@ -7,17 +7,17 @@ streamingmatch_F02<-function(alldocs=alldocs,resultados_reales=resultados_reales
     # filter(Grupo==Grupo_sm & Partido==Partido_sm)
     filter(Grupo=="W" & status_juego=="jugando")
   
-  show_tblsm<-reales_sm%>%select(Codigo2,Grupo,Partido)%>%distinct()%>%
+  show_tblsm<-reales_sm%>%select(Codigo2,Partido)%>%distinct()%>%
     # mutate(Goles_e1=0)%>%
     left_join(reales_sm%>%
                 filter(E1E2=="e1" | E1E2=="E1")%>%
-                select(Grupo,Partido,GolesFavor_real,Equipo_gsub)%>%
+                select(Partido,GolesFavor_real,Equipo_gsub)%>%
                 rename(Equipo1=Equipo_gsub)%>%
                 rename(Goles_e1=GolesFavor_real))%>%
     mutate(vs="vs")%>%
     left_join(reales_sm%>%
                 filter(E1E2=="e2" | E1E2=="E2")%>%
-                select(Grupo,Partido,Equipo_gsub,GolesFavor_real)%>%
+                select(Partido,Equipo_gsub,GolesFavor_real)%>%
                 rename(Equipo2=Equipo_gsub)%>%
                 rename(Goles_e2=GolesFavor_real))#%>%
     # mutate(Goles_e2=0)
@@ -25,7 +25,7 @@ streamingmatch_F02<-function(alldocs=alldocs,resultados_reales=resultados_reales
   prediccones_sm00<-alldocs%>%
     # filter(Grupo==Grupo_sm & Partido==Partido_sm)  
     right_join(reales_sm%>%
-                 select(Grupo,Partido,E1E2))
+                 select(Codigo2,Equipo_gsub))
   
   gbs_sm00<-generate_gbs_F02(prediccones_sm00,reales_sm)
   
